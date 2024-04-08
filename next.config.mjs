40c2127a40fdcc52a962879cab6b74ad30883de7
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-08 12:21:57 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
       },
       env: {
         NEXTAUTH_SECRET: "asddgd",
+        GOOGLE_CLIENT_ID:
+          "354984063009-qnvdallj4ro8aa91tk8ragrk9bqlj16k.apps.googleusercontent.com",
+        GOOGLE_CLIENT_SECRET: "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",
+        FACEBOOK_CLIENT_ID: "230771886171126",
+        FACEBOOK_CLIENT_SECRET: "1bee2330b2482066af92f6b2c5adee71",
         INSTAGRAM_KEY:
           "IGQWRNc252NHVWbzR3WnYwcmRrUHd2T0toT2MzeWlfMExFVGZAKU2ZADM3o5Tkw2RXNlcGFQdHp5b1ZAzdUxfN3pLTlBqUExob0RpZA1RxX3pHSGNKZA3o2SDYzRmg5RzhuQXlqRDFfMUdQaUU2ajFLQy1ydU9PV3BucXMZD",
         API_KEY: "AIzaSyBN4nK5weONPZ_TGutn6d-M_c5IT6nrAPg",
