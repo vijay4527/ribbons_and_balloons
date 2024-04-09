c429531ac141ee097b41dc635cb8c1dde78d71cf
vijay4527 <vijaysaroj4527@gmail.com>
2024-04-09 16:18:02 +0530

    carousel changes

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -37,6 +37,54 @@ const testimonial = () => {
     }, [session,isMounted]);
   return (
     <>
+     <Head>
+        <meta charset="utf-8"></meta>
+        {/* <title>Online Cake Delivery in Mumbai, Pune and Mangalore</title> */}
+        <meta
+          name="description"
+          content="Online Cakes Shop in Mumbai, Pune and Mangalore . Online Cakes Delivery . Buy,Order &amp; Send Birthday, Wedding Anniversary &amp; Chocolate Cakes anywhere in Mumbai from best Cake Shop Ribbons &amp; Balloons."
+        ></meta>
+        <meta
+          name="keywords"
+          content="Ribbons and Balloons, Buy Cakes Online, Online Cake delivery, Cakes Mumbai, Cakes to Mumbai, order cakes online, cake delivery in mumbai, Send Cakes to Mumbai, Mumbai Cake Shop, Online Cakes to Mumbai, Cakes Mumbai, Cake delivery to Mumbai, Chocolate Cakes Mumbai, Heart Shape Cakes, Eggless Cakes, Occasion Cakes, birthday cakes online delivery, Send Birthday Cakes, Congratulations Cakes, Missing You Cakes, Baby and Kids Cakes, Anniversary Cakes Online, Thank You Cakes, House Warming Cakes, Wedding Cakes Mumbai, customised cakes in mumbai, cup cakes mumbai, Online Cakes Shop Mumbai, valentine special cakes mumbai, plum cakes mumbai, fresh fruit cakes online"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        ></meta>
+        <link
+          rel="icon"
+          href="https://ribbonsandballoons.com/frontassets/images/fav.png"
+          type="image/x-icon"
+        />
+        <meta
+          name="google-site-verification"
+          content="hj44_Ud2995b4jkL3My7hTX96_sALd3yQ7tlf0El0IE"
+        ></meta>
+        <meta
+          name="p:domain_verify"
+          content="e35c0804c87b42f9187c00fa30ff64f9"
+        ></meta>
+        <meta
+          name="facebook-domain-verification"
+          content="1cpqqtudq8imtqkiwxpq0vd20x3b69"
+        ></meta>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        ></link>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+      </Head>
       <div className="testimonialsWrap">
     <Container fluid>
       <div className="testimonialsBody">
